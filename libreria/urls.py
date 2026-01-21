59a06f28c3fc8f943278decdccd13903198d3f32
Alberto <smb34abr@gmail.com>
2026-01-20 23:37:02 -0600

    Se agregaron las platillas

--- libreria/urls.py
+++ libreria/urls.py
@@ -4,4 +4,9 @@ from . import views
 urlpatterns = [
     path('', views.inicio, name='inicio'),
     path('nosotros/', views.nosotros, name='nosotros'),
-]
+    path('libros/', views.libros, name='libros'),    
+    path('crear_libro/', views.crear_libro, name='crear_libro'),
+    path('editar_libro/', views.editar_libro, name='editar_libro'),
+    path('eliminar_libro/', views.eliminar_libro, name='eliminar_libro'),
+    path('detalle_libro/', views.detalle_libro, name='detalle_libro'),
+]   
--- libreria/views.py
+++ libreria/views.py
@@ -7,3 +7,18 @@ def inicio(request):
 
 def nosotros(request):
     return render(request, 'paginas/nosotros.html')
+
+def libros(request):
+    return render(request, 'libros/index.html')
+
+def crear_libro(request):
+    return render(request, 'libros/crear.html') 
+
+def editar_libro(request):
+    return render(request, 'libros/editar.html')
+
+def eliminar_libro(request):
+    return render(request, 'libros/eliminar.html') 
+
+def detalle_libro(request):
+    return render(request, 'libros/detalle.html') 
